075347366be50c2cd4f8a5601056481c7c9359ce
Nik <Malzewn@gmail.com>
2024-10-28 21:37:49 +0300

    Изменил метод управления дроном, теперь можно проще управлять скоростью и рысканьем

--- FlightAlgorithm.py
+++ FlightAlgorithm.py
@@ -2,7 +2,8 @@ import rclpy
 from rclpy.node import Node
 from mavros_msgs.srv import SetMode
 from mavros_msgs.msg import State
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, TwistStamped
+import rclpy.time
 from std_msgs.msg import Header
 from sensor_msgs.msg import LaserScan
 import time
@@ -17,18 +18,14 @@ class TestNode(Node):
 
         self.set_mode_client = self.create_client(SetMode, '/uav1/mavros/set_mode')
 
-        # Публишер для установки целевой позиции дрона (для взлета)
-        self.local_pos_pub = self.create_publisher(PoseStamped, '/uav1/mavros/setpoint_position/local', 10)
-
-        # Подписываемся на топик состояния дрона (для проверки состояния) и лидара (для управление полетом)
-        self.state_sub = self.create_subscription(State, '/uav1/mavros/state', self.state_cb, 10)
+        
+        # Подписываемся на топик лидара (для управление полетом)
         self.lidar_sub = self.create_subscription(LaserScan, '/uav1/scan', self.lidar_cb, 10)
 
+        # Подписывваемся на паблишер для управления дроном 
+        self.cmd_vel_pub = self.create_publisher(TwistStamped, '/uav1/mavros/setpoint_velocity/cmd_vel', 10)
 
         self.test_services()
-
-    def state_cb(self, msg):
-        self.current_state = msg
 
     def test_services(self):
 
@@ -37,44 +34,23 @@ class TestNode(Node):
 
         time.sleep(2) # ждем некоторое время на высоте (например, 2 секунд)
 
-        #self.fly() # выполняем полет
-
-        time.sleep(2)
-
         # Посадка
         #self.land()
-
-    # Рысканье на deg градусов
-    def yaw(self, deg):
-        orientation = {'z':0.0,'w':0.0}
-        orientation['w'] = math.cos(np.radians(deg) / 2) 
-        orientation['z'] = math.sin(np.radians(deg) / 2)
-
-        return orientation
-
 
     def take_off(self):
         self.get_logger().info('\x1b[33m Попытка взлета... \x1b[0m ')
         # Создаем сообщение PoseStamped для взлета
-        pose = PoseStamped()
-        pose.header = Header()
+        pose = TwistStamped()
         pose.header.stamp = self.get_clock().now().to_msg()
 
         # Устанавливаем целевую позицию
-        pose.pose.position.x = 0.0
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 3.2
-
-        # Поворачиваемся на 90 градусов (ТЕСТ)
-        #orientation = self.yaw(90)
-        #pose.pose.orientation.w = orientation['w']
-        #pose.pose.orientation.z = orientation['z']
-        #pose.pose.orientation.x = 0.0
-        #pose.pose.orientation.y = 0.0
+        pose.twist.linear.x = 0.0
+        pose.twist.linear.y = 0.0
+        pose.twist.linear.z = 1.0
 
         # Публикуем целевую позицию несколько раз для взлета
         for _ in range(100):
-            self.local_pos_pub.publish(pose)
+            self.cmd_vel_pub.publish(pose)
             self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.05))
         self.get_logger().info('\x1b[33m Команда на взлет отправлена... \x1b[0m ')
 
@@ -94,63 +70,26 @@ class TestNode(Node):
                 self.get_logger().error('\x1b[31m Не удалось установить режим посадки \x1b[0m')
         else:
             self.get_logger().error('\x1b[31m Сервис установки режима недоступен \x1b[0m')
-
-        
-    def fly(self):
-        self.get_logger().info('\x1b[33m Попытка взлета... \x1b[0m ')
-
-        # Создаем сообщение PoseStamped для полета
-        pose = PoseStamped()
-        pose.header = Header()
-        pose.header.stamp = self.get_clock().now().to_msg()
-
-        # Пролет вперед на 2 метра
-        self.get_logger().info('\x1b[33m Пролет вперед на 2 метра...\x1b[0m')
-        pose.pose.position.x += 2.0
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 3.2
-
-
-        # Публикуем целевую позицию несколько раз для движения вперед
-        for _ in range(0, 100):
-            self.local_pos_pub.publish(pose)
-            self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.05))
-        self.get_logger().info('\x1b[32m Дрон пролетел вперед на 2 метра. \x1b[0m ')
-
-        # Пролет назад на 2 метра
-        self.get_logger().info('\x1b[33m Пролет назад на 2 метра...\x1b[0m')
-        pose.pose.position.x -= 2.0
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 3.2
-
-        # Публикуем целевую позицию несколько раз для движения назад
-        for _ in range(0, 100):
-            self.local_pos_pub.publish(pose)
-            self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.05))
-        self.get_logger().info('\x1b[32m Дрон пролетел вперед на 2 метра. \x1b[0m ')
-
     
-    def fly_forward(self, f_dist):
+    def fly_forward(self):
         self.get_logger().info('\x1b[33m Полет вперёд... \x1b[0m ')
 
         # Создаем сообщение PoseStamped для полета
-        pose = PoseStamped()
-        pose.header = Header()
+        pose = TwistStamped()
         pose.header.stamp = self.get_clock().now().to_msg()
 
         # Пролет вперед
-        self.get_logger().info(f'\x1b[33m Пролет вперед на {f_dist} метра...\x1b[0m')
-        pose.pose.position.x += f_dist-0.05
-        pose.pose.position.y = 0.0
-        pose.pose.position.z = 3.2
+        pose.twist.linear.x = 0.1
+        pose.twist.linear.y = 0.0
+        pose.twist.linear.z = 0.0
+        self.get_logger().info('\x1b[33m Пролет вперед...\x1b[0m')
 
 
         # Публикуем целевую позицию несколько раз для движения вперед
         for _ in range(0, 100):
-            #print(self.lidar_data)
-            self.local_pos_pub.publish(pose)
+            self.cmd_vel_pub.publish(pose)
             self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.05))
-        self.get_logger().info(f'\x1b[32m Дрон пролетел вперед на {f_dist} метра. \x1b[0m ')
+        self.get_logger().info('\x1b[32m Дрон пролетел вперед. \x1b[0m ')
 
           
 
@@ -161,12 +100,20 @@ class TestNode(Node):
         right_distance = min(msg.ranges[248:292])       # Лидар смотрит вправо
         #back_distance = min(msg.ranges[0:22][338:360]) # Лидар смотрит назад
         
+        pos = TwistStamped()
         
         # Двигаемся прямо, пока впереди нет препятствий
-        if front_distance > 0.2 and front_distance != float('inf'):  # Если впереди нет препятствия ближе 0.7 метра
-            self.fly_forward(front_distance) # Лети вперёд до препятствия
+        if front_distance > 1.0 and left_distance > 1.0 and right_distance > 1.0 and front_distance != float('inf'):  # Если впереди нет препятствия ближе 0.7 метра
+            self.fly_forward() # Лети вперёд до препятствия
         else:
             # Останавливаемся, если впереди препятствие
+            pos.twist.linear.x = 0.0
+            pos.twist.linear.y = 0.0
+            pos.twist.linear.z = 0.0
+
+            for _ in range(0, 100):
+                self.cmd_vel_pub.publish(pos)
+                self.get_clock().sleep_for(rclpy.time.Duration(seconds=0.05))
             self.land()
 
 
